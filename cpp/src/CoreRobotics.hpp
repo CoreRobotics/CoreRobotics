d52422b23f6f1ef9e2e48a6ccebafa1e899b89c1
Parker Owan <owan@uw.edu>
2017-04-19 16:30:31 -0700

    Updated the documentation to include how to install for windows and a section for the licesne.  Updated some general descriptions in getting started

--- cpp/src/CoreRobotics.hpp
+++ cpp/src/CoreRobotics.hpp
@@ -46,14 +46,17 @@
 //===========================================================================
 /*! \mainpage Introduction
 
-The CoreRobotics libraries provide packages for real-time robot control
-including kinematics, estimation, percetion, modeling and interfacing with
+The CoreRobotics libraries provide basic modules for real-time robot control
+including kinematics, estimation, perception, modeling and representations of
 items in the world. The libraries are written in C++ and are intended to be
-cross-platform, although they have currently only been tested on Windows 8.1
-and Mac OS X platforms.  The code is distributed under the BSD 3-clause
-license for flexibility of use.
+cross-platform, although they have currently only been tested on Windows 
+8.1/10, Mac OS X, and Ubuntu platforms.  The code is distributed under the
+BSD 3-clause license for flexibility of use.  By using this software, you are
+agreeing to the license.  Please read the license prior to installing the
+CoreRobotics library.
  
 This introduction is broken down into the following sections.
+- \subpage license
 - \subpage install
 - \subpage overview
 - \subpage gettingstarted
@@ -62,6 +65,45 @@ This introduction is broken down into the following sections.
 
 A note on units:  All units in the library, unless specified, are in SI
 (international standard), i.e.: radians, meters, kilograms, etc...
+ 
+ 
+*/
+//---------------------------------------------------------------------------
+/*!
+\page license Software License
+
+Software License Agreement (BSD-3-Clause License)\n
+Copyright (c) 2017, CoreRobotics.\n
+All rights reserved.\n\n
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:\n
+
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+
+3. Neither the name of CoreRobotics nor the names of its contributors
+may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+ 
 */
 //---------------------------------------------------------------------------
 /*!
@@ -70,8 +112,12 @@ A note on units:  All units in the library, unless specified, are in SI
 The CoreRobotics Library comes with CMakeLists ready to generate the project
 for your IDE of choice.  CMake is a tool for generating platform-specific 
 IDE files from cross-platform source code.  We highly recommend the use of 
-CMake to build your first program.
-\n\n\n\n
+CMake to build your first program.  Instructions for how to build CoreRobotics
+for your specific platform using CMake are presented below.
+- \subpage install_mac
+- \subpage install_windows
+- \subpage install_unix
+\n\n
 
 \section install_mac Mac XCode
 Tested on Mac OSX 10.11.6 with XCode Version 8.1.  The remaining steps assume
@@ -108,28 +154,62 @@ following terminal commands (assuming you left terminal alone from step 2)
 \code
 $ ../utils/bin/Debug/TestModules
 \endcode
- 
-### Step 5.
-Check out the tutorials to begin making your first program in CoreRobotics.
 \n\n\n\n
  
 \section install_windows Windows Visual Studio
-Todo
+Tested on Windows 8.1 with Microsoft Visual Studio 2015.  The remaining steps
+assume you have already installed Visual Studio 2015 on your PC.
 
+### Step 1.
+Download the latest version of CMake from https://cmake.org/download/
+for Windows (*.msi).  Install CMake by running the *.msi.  Once CMake has
+installed, it is accesible via GUI or the command line.
+
+### Step 2.
+Once CMake is set up for command line use, open terminal and cd into
+the root folder of CoreRobotics.  There should be subfolders named doc, src,
+utils, etc.  Run the following commands one at a time to build the CoreRobotics
+library, test routines, and example code:
+
+\code
+> mkdir build
+> cd build
+> cmake -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 14 2015" ../
+> CoreRobotics.sln
+\endcode
+
+### Step 3.
+After the project files are created, the CoreRobotics Visual Studio project
+should open.  Go to Build >> Build Solution (F7) to build the project.
+
+### Step 4.
+Once the project has built, you can run the test script by running the
+following terminal commands (assuming you left terminal alone from step 2)
+
+\code
+> ..\utils\bin\Release\TestModules.exe
+\endcode
 \n\n\n\n
+
  
-\section install_make Linux Make
+\section install_unix Unix Makefiles
 Todo
 */
 //---------------------------------------------------------------------------
 /*!
 \page overview Overview
-Todo
-*/
+CoreRobotics is seperated into several modules, based on the natural
+classifications of robotics literature and implementation.  The intention is
+for each module to solve different problems in robotics applications.  Here
+we present a light overview of the modules and briefly describe the use for
+each class.
+ */
 //---------------------------------------------------------------------------
 /*!
 \page gettingstarted Getting Started
-Todo
+This section assumes you have properly installed CoreRobotics for your 
+platform.  It is also recommended that you've read through the Overview
+section prior to tackling some of the examples presented here.
 
 \section transformations Transformations
 \section kinematics Kinematics
