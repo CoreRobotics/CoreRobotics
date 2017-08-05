b14fdecde1899af5c470d28d3000a55778f924f8
Cameron Devine <camerondtrain@gmail.com>
2017-08-04 20:27:03 -0700

    Added documentation and license to the nullspace controller.

--- cpp/src/controllers/CRNullSpace.cpp
+++ cpp/src/controllers/CRNullSpace.cpp
@@ -1,8 +1,67 @@
+//=====================================================================
+/*
+ Software License Agreement (BSD-3-Clause License)
+ Copyright (c) 2017, CoreRobotics.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of CoreRobotics nor the names of its contributors
+ may be used to endorse or promote products derived from this
+ software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ \project CoreRobotics Project
+ \url     www.corerobotics.org
+ \author  Cameron Devine
+ \version 0.0
+ 
+ */
+//=====================================================================
+
 #include "CRNullSpace.hpp"
 #include "CRMath.hpp"
 
+//=====================================================================
+// CoreRobotics namespace
 namespace CoreRobotics {
 
+
+//=====================================================================
+/*!
+ The constructor creates an nullspace solver.  Specifically,
+ the algorithm uses an orthogonal projection matrix.  The SVD is
+ used to compute the Jacobian generalized inverse.\n
+ 
+ \param[in]     i_robot         the CoreRobotics::CRManipulator object 
+                                to be used for solving for the null space
+ \param[in]     i_toolIndex     the index of the robot tool for which 
+                                the IK is being solved, see 
+                                CoreRobotics::CRManipulator::addTool()
+ \param[in]     i_eulerMode     the Euler convention of the pose vector
+ */
+//---------------------------------------------------------------------
 CRNullSpace::CRNullSpace(CRManipulator* i_robot,
                          unsigned int i_toolIndex,
                          CREulerMode i_eulerMode)
@@ -13,6 +72,21 @@ CRNullSpace::CRNullSpace(CRManipulator* i_robot,
 	this->setSingularThresh(1.0e-1);
 }
 
+
+//=====================================================================
+/*!
+
+ This method computes the joint velocities that produce no tool movement.
+ Desired joint valocities are specified via i_velocities, and an initial
+ condition for the joint angles is specified via i_q0. The method returns
+ a vector of joint velocities in the nullspace of the jacobian.\n
+ 
+ \param[in]     i_velcities     the desired joint velocities
+ \param[in]     i_q0            the intial configuration of the robot
+ \return                        the joint velcities in the nullspace of
+                                robot jacobian
+ */
+//---------------------------------------------------------------------
 Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
                                    Eigen::VectorXd i_q0)
 {
@@ -24,6 +98,24 @@ Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
 	return (Eigen::MatrixXd::Identity(this->m_robot->getDegreesOfFreedom(), this->m_robot->getDegreesOfFreedom()) - (Jinv * J)) * i_velocities;
 }
 
+//=====================================================================
+/*!
+
+ This method computes the joint velocities that produce no tool movement
+ in the directions specified by i_poseElements.  Desired joint valocities
+ are specified via i_velocities, and an initial condition for the joint
+ angles is specified via i_q0. The method returns a vector of joint velocities
+ in the nullspace of the jacobian.\n
+ 
+ \param[in]     i_velcities     the desired joint velocities
+ \param[in]     i_poseElements  a boolean vector indiciating which
+                                elements of the pose vector are specified
+							    in i_setPoint (see CRFrame::getPose)
+ \param[in]     i_q0            the intial configuration of the robot
+ \return                        the joint velcities in the nullspace of
+                                robot jacobian
+ */
+//---------------------------------------------------------------------
 Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
                                    Eigen::Matrix<bool, 6, 1> i_poseElements,
                                    Eigen::VectorXd i_q0)
@@ -37,6 +129,24 @@ Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
 	return (Eigen::MatrixXd::Identity(this->m_robot->getDegreesOfFreedom(), this->m_robot->getDegreesOfFreedom()) - (Jinv * J)) * i_velocities;
 }
 
+//=====================================================================
+/*!
+
+ This method computes the joint velocities that produce no tool movement
+ in the directions specified by i_poseElements.  Desired joint valocities
+ are specified via i_velocities, and an initial condition for the joint
+ angles is specified via i_q0. The method returns a vector of joint velocities
+ in the nullspace of the jacobian.\n
+ 
+ \param[in]     i_velcities     the desired joint velocities
+ \param[in]     i_poseElements  a integer vector indiciating which
+                                elements of the pose vector are specified
+							    in i_setPoint (see CRManipulator::jacobian)
+ \param[in]     i_q0            the intial configuration of the robot
+ \return                        the joint velcities in the nullspace of
+                                robot jacobian
+ */
+//---------------------------------------------------------------------
 Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
                                    Eigen::Matrix<int, 6, 1> i_poseElementsInt,
                                    Eigen::VectorXd i_q0)
@@ -45,4 +155,6 @@ Eigen::VectorXd CRNullSpace::solve(Eigen::VectorXd i_velocities,
 	return CRNullSpace::solve(i_velocities, i_poseElements, i_q0);
 }
 
+//=====================================================================
+// End namespace
 }
--- cpp/src/controllers/CRNullSpace.hpp
+++ cpp/src/controllers/CRNullSpace.hpp
@@ -1,31 +1,152 @@
+//=====================================================================
+/*
+ Software License Agreement (BSD-3-Clause License)
+ Copyright (c) 2017, CoreRobotics.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ 
+ * Neither the name of CoreRobotics nor the names of its contributors
+ may be used to endorse or promote products derived from this
+ software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ \project CoreRobotics Project
+ \url     www.corerobotics.org
+ \author  Cameron Devine
+ \version 0.0
+ 
+ */
+//=====================================================================
+
 #ifndef CRNullSpace_hpp
 #define CRNullSpace_hpp
 
+//=====================================================================
+// Includes
 #include "CRManipulator.hpp"
 #include "CRTypes.hpp"
 
+//=====================================================================
+/*!
+ \file CRNullSpace.hpp
+ \brief Implements a class to solve manipulator nullspace movements.
+ */
+//---------------------------------------------------------------------
+/*!
+ \class CRNullSpace
+ \ingroup controllers
+ 
+ \brief This class provides methods for solving the manipulator nullspace
+ control problem.
+ 
+ \details
+ ## Description
+
+ CRNullSpace implements the orthogonal projection matrix to find a vector
+ in the nullspace of the Jacobian given an arbetrary vector of joint space
+ velocities.  The SVD based generalized inverse function in CRMath is used
+ to find the sudoinverse of the Jacobian.
+
+ CRNullSpace find the orthogonal projection of the arbetrary joint space
+ vector usingthe following equation,
+ 
+ \f$ \dot{q}_{null} = (I - J^\dagger J) \dot{q}_0 \f$,
+ 
+ where \f$\dot{q}_0\f$ is the desired joint velocities, \f$J\f$ is the robot
+ jacobian, and \f$\dot{q}_{null}\f$ is a set of joint velocities in the nullspace
+ of the robot jacobian.
+ 
+ These methods are used to interface with the nullspace controller:
+ - CRNullSpace::setRobot sets the manipulator nullspace to solve
+ - CRNullSpace::setToolIndex set the tool index of the 
+ associated manipulator (see CRManipulator::addTool) for which we are
+ setting a desired nullspace command.  Note that a tool must be added in the manipulator
+ prior to using the nullspace solver.
+ - CRNullSpace::setEulerMode sets the Euler convention of the pose
+ vector
+ - CRNullSpace::solve solves the nullspace control problem described above for
+ an initial configuration and a desired set of joint velocities.  The method is
+ overloaded to allow for solving for a reduced pose vector (see CRFrame::getPose)
+ 
+ Optimization parameters can be set:
+ - CRNullSpace::setSingularThresh sets the threshold for numerically
+ determining if the Jacobian is singular by comparing singular values of
+ the SVD to the threshold. (Default = 0.1)
+ 
+ 
+ ## Example
+ This example demonstrates use of the CRNullSpace class.
+ \include test_CRNullSpace.cpp
+ 
+ ## References
+ [3] B. Siciliano and O. Khatib, "Springer Handbook of Robotics",
+ Springer, 2016.\n\n 
+ 
+ */
+//=====================================================================
 namespace CoreRobotics {
 
+//---------------------------------------------------------------------
+// Constructor and Destructor
 class CRNullSpace {
 
 public:
 
+	//! Class constructor
 	CRNullSpace(CRManipulator* i_robot,
                 unsigned int i_toolIndex,
                 CREulerMode i_eulerMode);
 
+//---------------------------------------------------------------------
+// Get/Set Methods
+public:
+
+	//! Set the robot to be used
 	void setRobot(CRManipulator* i_robot) {this->m_robot = i_robot;}
 
+	//! Set robot tool index to use to compute the nullspace.  Note that a tool must be specified in the robot.
 	void setToolIndex(unsigned int i_toolIndex) {this->m_toolIndex = i_toolIndex;}
 
+	//! Set the Euler angle convention of the IK solver.  This must match the
+    //  Euler convention used to supply the set point in the solve() method.
 	void setEulerMode(CREulerMode i_eulerMode) {this->m_eulerMode = i_eulerMode;}
 
+	//! Get the Euler convention
 	CREulerMode getEulerMode(void) {return this->m_eulerMode;}
 
+	//! Set the minimum threshold for a non-singular matrix
 	void setSingularThresh(double i_thresh) {this->m_svdTol = i_thresh;}
 
+	//! Get the minimum threshold for a non-singular matrix
 	double getSingularThresh(void) {return this->m_svdTol;}
 
+//---------------------------------------------------------------------
+// Public Methods
+public:
+
+	//! Solve for the joint velocities that yield no tool motion, or tool motion only in the degrees of freedom specified in i_poseElements
 	Eigen::VectorXd solve(Eigen::VectorXd i_velocities,
                           Eigen::VectorXd i_q0);
 
@@ -37,18 +158,26 @@ public:
                           Eigen::Matrix<int, 6, 1> i_poseElementsInt,
                           Eigen::VectorXd i_q0);
 
+//---------------------------------------------------------------------
+// Protected Members
 protected:
 
+	//! Manipulator object to solve
 	CRManipulator* m_robot;
 
+	//! Index of the manipulator tool for which to solve the IK
 	unsigned int m_toolIndex;
 
+	//! Euler Convention to use
 	CREulerMode m_eulerMode;
 
+	//! Tolerance for computing if a matrix is singular using SVD svals
 	double m_svdTol;
 
 };
 
+//=====================================================================
+// End namespace
 }
 
 #endif
