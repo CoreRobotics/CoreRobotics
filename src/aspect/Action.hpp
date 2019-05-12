118be516c989626116a21b8e1bb6f56f17700e78
Parker Owan <owan@uw.edu>
2019-05-12 01:06:59 -0700

    added doxygen to action aspect

--- src/aspect/Action.hpp
+++ src/aspect/Action.hpp
@@ -4,20 +4,38 @@
  * Licensed under BSD-3, https://opensource.org/licenses/BSD-3-Clause
  */
 
-//! Classes that have a read only action aspect should include this macro
+/*!
+  \def CR_ASPECT_ACTION_READ(ActionType)
+  Classes that have a read only action aspect should include this macro.
+
+  \brief This macro adds class members.
+  \param ActionType  The action data type.
+*/
 #define CR_ASPECT_ACTION_READ(ActionType) \
 public: \
   const ActionType& getAction() const { return m_action; } \
 protected: \
   ActionType m_action; \
 
-//! Classes that have a read/write action aspect should include this macro
+/*!
+  \def CR_ASPECT_ACTION_WRITE(ActionType)
+  Classes that have a read/write action aspect should include this macro
+
+  \brief This macro adds class members.
+  \param ActionType  The action data type.
+*/
 #define CR_ASPECT_ACTION_WRITE(ActionType) \
 CR_ASPECT_ACTION_READ(ActionType) \
 public: \
   void setAction(const ActionType& i_x) { m_action = i_x; } \
 
-//! Classes that have a mutable action aspect should include this macro
+/*!
+  \def CR_ASPECT_ACTION_MUTABLE(ActionType)
+  Classes that have a mutable action aspect should include this macro
+
+  \brief This macro adds class members.
+  \param ActionType  The action data type.
+*/
 #define CR_ASPECT_ACTION_MUTABLE(ActionType) \
 CR_ASPECT_ACTION_WRITE(ActionType) \
 public: \
